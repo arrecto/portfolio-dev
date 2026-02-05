ae4ffea9726a35e02906ba9f693ebc2397c7dff4
allenivanrecto-work <allenivanrecto.work@gmail.com>
2026-02-05 16:23:51 +0800

    fix workflows

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/portfolio-dev" : "",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
